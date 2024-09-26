fdf85289b477739215c042c4e7e9bc485327dde9
playgamesgo <pplaygamesgo@gmail.com>
2024-09-26 18:09:59 +0200

    user GET DELETE PUT endpoint

---
+++ Backend/src/main/java/me/playgamesgo/petworldbackend/payload/response/UserResponse.java
@@ -0,0 +1,26 @@
+package me.playgamesgo.petworldbackend.payload.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UserResponse {
+    long id;
+    String firstName;
+    String lastName;
+    String email;
+    String location;
+    Date createdAt;
+
+    public UserResponse(long id, String firstName, String lastName, String email, String location, Date createdAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.location = location;
+        this.createdAt = createdAt;
+    }
+}
---
+++ Backend/src/main/java/me/playgamesgo/petworldbackend/services/UserService.java
@@ -0,0 +1,35 @@
+package me.playgamesgo.petworldbackend.services;
+
+import me.playgamesgo.petworldbackend.models.User;
+import me.playgamesgo.petworldbackend.payload.request.UpdateUserRequest;
+import me.playgamesgo.petworldbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void updateUser(Long id, UpdateUserRequest userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) return;
+        if (userDetails.getFirstName() != null) user.setFirstName(userDetails.getFirstName());
+        if (userDetails.getLastName() != null) user.setLastName(userDetails.getLastName());
+        if (userDetails.getEmail() != null) user.setEmail(userDetails.getEmail());
+        if (userDetails.getLocation() != null) user.setLocation(userDetails.getLocation());
+        if (userDetails.getPassword() != null) user.setPassword(userDetails.getPassword());
+        userRepository.save(user);
+    }
+}
